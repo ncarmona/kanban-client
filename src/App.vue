730f474825c56f39ee8dea4a5cc5a9fc8af1a662
Noel Carmona <hello@noelcarmona.com>
2022-02-28 17:49:12 +0100

    [DEPENDENCIES] Installed tailwind

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1 class="text-red-700">Hello</h1>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
