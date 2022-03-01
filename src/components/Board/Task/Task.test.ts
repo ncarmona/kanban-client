40b3e91e74fbe90514700e9294127314593141b1
Noel Carmona <hello@noelcarmona.com>
2022-03-01 17:55:28 +0100

    [BOARD] Task component implementation.

---
+++ src/components/Board/Task/Task.test.ts
@@ -0,0 +1,56 @@
+import TaskComponent from './TaskComponent.vue'
+import { DOMWrapper, mount } from '@vue/test-utils'
+import { ITask } from 'domain/interfaces/ITask'
+import { ITag } from 'domain/interfaces/ITag'
+import TaskTagListComponent from '../TaskTagListComponent/TaskTagListComponent.vue'
+
+describe("Task component", () => {
+  const tag2: ITag = {
+    id: "2",
+    name: "tag2",
+    color: "bg-orange-500",
+    board: "1",
+    createdAt: new Date(),
+    modifiedAt: new Date(),
+    deleted: false,
+    disabled: false
+  }
+  const tag3: ITag = {
+    id: "1",
+    name: "tag3",
+    color: "bg-orange-500",
+    board: "1",
+    createdAt: new Date(),
+    modifiedAt: new Date(),
+    deleted: false,
+    disabled: false
+  }
+  const task:ITask = {
+      id: "1",
+      title: "Task1",
+      description: "Task 1 description",
+      author: "noel",
+      tags: [tag2, tag3],
+      backlog: false,
+      deleted: false,
+      disabled: false,
+      modifiedAt: new Date(),
+      createdAt: new Date(),
+      column: "1"
+    }
+  const wrapper = mount(TaskComponent, { props: { task } })
+  const spanTexts = wrapper.findAll('span').map((e:DOMWrapper<HTMLSpanElement>) => e.text())
+  it("Task title rendered", () => {
+    const titleRendered:boolean = spanTexts.some((t:string) => t === task.title)
+    expect(titleRendered).toBeTruthy()
+  })
+  it("Task description rendered", () => {
+    const descriptionRendered:boolean = spanTexts.some((t:string) => t === task.description)
+    expect(descriptionRendered).toBeTruthy()
+  })
+  it("Task tags rendered", () => {
+    const taskTagListRendered = wrapper.findComponent(TaskTagListComponent).exists()
+
+    expect(taskTagListRendered).toBeTruthy()
+  })
+})
---
+++ src/components/Board/Task/TaskComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col justify-items-center border border-gray-200">
+    <span> {{task.title}} </span>
+    <span> {{description}} </span>
+    <div>
+      <TaskTagList :tags="task.tags" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ITask } from 'domain/interfaces/ITask'
+import { computed, defineComponent, toRefs } from 'vue'
+import TaskTagList from '../TaskTagListComponent/TaskTagListComponent.vue'
+
+export default defineComponent({
+  props: {
+    task: {
+      required: true,
+      type: Object as () => ITask
+    }
+  },
+  components: { TaskTagList },
+  setup(props) {
+    const description = computed(() => props.task.description)
+    return {...toRefs(props), description}
+  }
+})
+</script>
