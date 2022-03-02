2bb3df7025ffacbe58c354c250f53a88f78e0f57
Noel Carmona <hello@noelcarmona.com>
2022-03-02 18:48:47 +0100

    [BOARD] Toggle tags task component

---
+++ src/components/Board/ToggleTagsTask/ToggleTagsTaskComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <Component :is="icon" class="h-5"/>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs } from 'vue'
+import { CogIcon, HashtagIcon } from '@heroicons/vue/outline'
+
+export default defineComponent({
+  components: { CogIcon, HashtagIcon },
+  props: {
+    icon: {
+      required: true,
+      type: String
+    }
+  },
+  setup(props) {
+    return { ...toRefs(props) }
+  }
+})
+</script>
