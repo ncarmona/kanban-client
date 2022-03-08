8d78b7a9d1d355771e38b7167c0b0329527dd07c
Noel Carmona <hello@noelcarmona.com>
2022-03-08 11:48:39 +0100

    [NAVBAR] Removed toggle icon

--- src/components/UI/NavBar/NavBarToggler/NavBarToggleComponent.vue
+++ src/components/UI/NavBar/NavBarToggler/NavBarToggleComponent.vue
@@ -1,23 +1,19 @@
 <template>
   <div class="flex text-white cursor-pointer hover:bg-violet-500">
-    <Component :is="icon" class="flex text-white cursor-pointer hover:bg-violet-500 h-6" />
+    <MenuIcon class="flex text-white cursor-pointer hover:bg-violet-500 h-6" />
     <span>{{text}}</span>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
-import { MenuIcon, XIcon } from '@heroicons/vue/solid'
+import { defineComponent } from 'vue'
+import { MenuIcon } from '@heroicons/vue/solid'
 
 export default defineComponent({
   components: { MenuIcon },
-  props: {
-    toggler: { required: true }
-  },
   setup(props) {
     const text = "Menu"
-    const icon = computed(() => props.toggler ? XIcon : MenuIcon)
-    return { text, icon }
+    return { text }
   }
 })
 </script>
