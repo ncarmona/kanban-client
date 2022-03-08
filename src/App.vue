6ee762bf1d5cd4a0bb622f56af656bad422f0fc2
Noel Carmona <hello@noelcarmona.com>
2022-03-08 11:51:18 +0100

    [APP] Added NavBar to App component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
-<script lang="ts">
-import { defineComponent } from '@vue/runtime-core';
-import BoardComponent from './components/Board/BoardComponent.vue';
-
-export default defineComponent({
-  components: { BoardComponent }
-})
-</script>
-
 <template>
-  <div class="h-screen pt-20 bg-gray-50">
-    <BoardComponent />
+  <div class="h-screen bg-gray-50">
+    <NavBarComponent class="bg-violet-700 shadow-xl" :navBarItems="menuItems"/>
+    <div class="pt-20">
+      <BoardComponent />
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { AdjustmentsIcon, ArchiveIcon, DocumentAddIcon, LogoutIcon, UserIcon, ViewBoardsIcon } from '@heroicons/vue/solid';
+import { defineComponent } from '@vue/runtime-core';
+import BoardComponent from './components/Board/BoardComponent.vue';
+import NavBarComponent from './components/UI/NavBar/NavBarComponent.vue'
+import { INavBar } from './components/UI/NavBar/interfaces/NavBar'
+
+export default defineComponent({
+  components: { BoardComponent, NavBarComponent },
+  setup() {
+    const menuItems: INavBar = {
+      left: [
+        { text: "Create task", icon: DocumentAddIcon, type: "link" },
+        { text: "Backlog", icon: ArchiveIcon, type: "link" },
+        { text: "Board configuration", icon: AdjustmentsIcon, type: "link" },
+      ],
+      right: [
+        { text: "Boards", icon: ViewBoardsIcon, type: "menu", subLinks: [
+          { text: "Submenu1" },
+          { text: "Submenu2" },
+          { text: "Submenu3" }
+        ]},
+        { text: "User", icon: UserIcon, type: "menu", subLinks: [
+          { text: "Submenu1" },
+          { text: "Submenu2" }
+        ]},
+        { text: "Logout", icon: LogoutIcon, type: "link" }
+      ]
+    }
+    return { menuItems }
+  }
+})
+</script>
