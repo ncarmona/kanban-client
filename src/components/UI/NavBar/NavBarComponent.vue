ea885fd1fbedbfaabea8f6ba6a158d60f605f712
Noel Carmona <hello@noelcarmona.com>
2022-03-09 17:12:16 +0100

    [NAVBAR] Navbar component has navlinks  and navsublinks grouped.

--- src/components/UI/NavBar/NavBarComponent.vue
+++ src/components/UI/NavBar/NavBarComponent.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="md:flex text-white">
+  <div class="text-white">
     <NavBarToggler class="md:hidden p-3" @click="toggler"/>
-    <div ref="navBar" class="md:flex md:grow md:mt-0 md:pb-0 mt-5 pb-3 hidden">
-      <div v-for="item in navBarItems.left" :key="item.text">
-        <NavBarLink class="p-3" v-if="isLink(item.type)" :text="item.text" :icon="item.icon" />
-        <NavBarSubMenu class="p-3" v-if="isSubMenu(item.type)" :text="item.text" :icon="item.icon" :subLinks="item.subLinks" />
+    <div ref="navBar" class="md:flex md:mt-0 md:pb-0 mt-5 pb-3 hidden">
+      <div class="md:flex md:justify-start md:grow">
+        <NavBarGroup :items="navBarItems.left" />
       </div>
       <hr class="mt-4 mb-4"/>
-      <div v-for="item in navBarItems.right" :key="item.text">
-        <NavBarLink class="p-3" v-if="isLink(item.type)" :text="item.text" :icon="item.icon" />
-        <NavBarSubMenu class="p-3" v-if="isSubMenu(item.type)" :text="item.text" :icon="item.icon" :subLinks="item.subLinks" />
+      <div class="md:flex md:justify-items-end">
+        <NavBarGroup :items="navBarItems.right" />
       </div>
     </div>
   </div>
@@ -17,12 +15,11 @@
 
 <script lang="ts">
 import { defineComponent, onBeforeMount, Ref, ref, toRefs } from 'vue'
-import NavBarLink from './NavBarLink/NavBarLinkComponent.vue'
-import NavBarSubMenu from './NavBarSubMenu/NavBarSubMenuComponent.vue'
 import NavBarToggler from './NavBarToggler/NavBarToggleComponent.vue'
+import NavBarGroup from './NavBarGroup/NavBarGroupComponent.vue'
 
 export default defineComponent({
-  components: { NavBarLink, NavBarSubMenu, NavBarToggler },
+  components: { NavBarToggler, NavBarGroup },
   props: {
     navBarItems: {
       type: Object,
@@ -32,14 +29,12 @@ export default defineComponent({
   setup(props) {
     const navBar:Ref<any> = ref(null)
 
-    const isLink = (type: string) => type === 'link'
-    const isSubMenu = (type: string) => type === 'menu'
     const toggler = () => navBar.value.classList.toggle('hidden')
     const closeMenu = () => navBar.value.classList.add('hidden')
 
     onBeforeMount(() => window.addEventListener('resize', () => closeMenu()))
 
-    return { ...toRefs(props), toggler, isLink, isSubMenu, navBar }
+    return { ...toRefs(props), toggler, navBar }
   }
 })
 </script>
