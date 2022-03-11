2e554f81742bf1ece7f8f6573338ca0565c5189a
Noel Carmona <hello@noelcarmona.com>
2022-03-11 18:10:32 +0100

    [ROUTER] Added Board view with registered user layout

---
+++ src/views/BoardView.vue
@@ -0,0 +1,15 @@
+<template>
+  <RegisteredUserLayout>
+    <BoardComponent />
+  </RegisteredUserLayout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core'
+import BoardComponent from '../components/Board/BoardComponent.vue'
+import RegisteredUserLayout from '../layouts/RegisteredUserLayout.vue'
+
+export default defineComponent({
+  components: { BoardComponent, RegisteredUserLayout }
+})
+</script>
