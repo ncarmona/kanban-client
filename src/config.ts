b6a6e865005008934d2d269e30bd455aa59b375b
Noel Carmona <hello@noelcarmona.com>
2022-03-13 11:09:52 +0100

    [ROUTER] Removed route import warning

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ import { RouteRecordRaw } from 'vue-router'
 
 export const routes: RouteRecordRaw[] = [{
   path: '/',
-  component: import(/* webpackChunkName: "board" */ './views/BoardView.vue')
+  component: () => import(/* webpackChunkName: "board" */ './views/BoardView.vue')
 }]
