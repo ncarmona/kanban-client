2c7efba83ed43297e0fe3b91d0ba81af767c228c
Noel Carmona <hello@noelcarmona.com>
2022-03-13 11:11:10 +0100

    [BOARD] Assigned column name as key in board column compoennt.

--- src/components/Board/BoardComponent.vue
+++ src/components/Board/BoardComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-items-center">
-    <BoardColumnComponent class="ml-14" v-for="column in board.columns" :column="column" :key="column.id"/>
+    <BoardColumnComponent class="ml-14" v-for="column in board.columns" :column="column" :key="column.name"/>
   </div>
 </template>
 <script lang="ts">
