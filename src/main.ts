2af2678155945452aa21737205d34b8be00e3f4f
Noel Carmona <hello@noelcarmona.com>
2022-03-29 13:36:52 +0200

    [VUEX] Added created store to vue instance

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import Router from './core/router'
+import store from './store/store'
 
 import App from './App.vue'
 import './assets/tailwind.css'
 
 createApp(App)
+.use(store)
 .use(Router)
 .mount('#app')
