4a3f4d48530dd4f89d183e901539d9a113a1b002
Noel Carmona <hello@noelcarmona.com>
2022-03-30 17:36:04 +0200

    [NAVBAR] Submenu supports router-link and regular links

--- src/components/UI/NavBar/NavBarSubMenu/NavBarSubMenuComponent.vue
+++ src/components/UI/NavBar/NavBarSubMenu/NavBarSubMenuComponent.vue
@@ -6,7 +6,8 @@
     </div>
     <div class="submenu mt-2 md:pl-0 md:flex md:flex-col pl-5 delay-75 duration-300 md:bg-white md:rounded md:shadow-md md:absolute md:z-10 md:top-11" v-show="menuVisible">
       <div class="md:text-gray-700 p-3 hover:bg-violet-700 hover:text-white" v-for="link in subLinks" :key="link.text">
-        <span class="submenu-item">{{link.text}}</span>
+        <a v-if="isStringURL(link.url)" :href="link.url" class="submenu-item">{{link.text}}</a>
+        <router-link v-else :to="link.url">{{link.text}}</router-link>
       </div>
     </div>
   </div>
@@ -16,6 +17,8 @@
 import { computed, defineComponent, ref, Ref, toRefs } from 'vue'
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/vue/solid' 
 import NavBarLink from '../NavBarLink/NavBarLinkComponent.vue'
+import { INavBarSubLink } from '../interfaces/NavBarSubLink'
+import { RouteLocationRaw } from 'vue-router'
 
 export default defineComponent({
   components: { ChevronDownIcon, ChevronUpIcon ,NavBarLink },
@@ -30,14 +33,15 @@ export default defineComponent({
     },
     subLinks: {
       required: true,
-      type: Object
+      type: Object as () => INavBarSubLink[] | RouteLocationRaw[]
     }
   },
   setup(props) {
     const menuVisible:Ref<boolean> = ref(false)
     const toggleSubmenu = () => menuVisible.value = !menuVisible.value
     const chevronIcon = computed(() => menuVisible.value ? ChevronUpIcon : ChevronDownIcon)
-    return { ...toRefs(props), toggleSubmenu, menuVisible, chevronIcon}
+    const isStringURL = (link: INavBarSubLink | string ): boolean => typeof link === 'string'
+    return { ...toRefs(props), toggleSubmenu, menuVisible, chevronIcon, isStringURL }
   },
 })
 </script>
