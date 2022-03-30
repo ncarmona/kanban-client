5299e3f78d1cdb2d6631533bc4aad4627ecf23a7
Noel Carmona <hello@noelcarmona.com>
2022-03-30 10:35:18 +0200

    [ROUTER] Added name to routes

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,14 @@
 import { RouteRecordRaw } from 'vue-router'
 
-export const routes: RouteRecordRaw[] = [{
-  path: '/board/:board',
-  component: () => import(/* webpackChunkName: "board" */ './views/BoardView.vue')
-}]
+export const routes: RouteRecordRaw[] = [
+  {
+    name: 'index',
+    path: '/',
+    component: () => import(/* webpackChunkName: "boardsList" */ './views/BoardsListView.vue')
+  },
+  {
+    name: 'board',
+    path: '/board/:board',
+    component: () => import(/* webpackChunkName: "board" */ './views/BoardView.vue')
+  }
+]
