d13e44dc8e174461b90411b889f686cb6954bb14
Noel Carmona <hello@noelcarmona.com>
2022-03-30 10:37:00 +0200

    [ROUTER] Removed index route

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,6 @@ import { RouteRecordRaw } from 'vue-router'
 
 export const routes: RouteRecordRaw[] = [
   {
-    name: 'index',
-    path: '/',
-    component: () => import(/* webpackChunkName: "boardsList" */ './views/BoardsListView.vue')
-  },
-  {
     name: 'board',
     path: '/board/:board',
     component: () => import(/* webpackChunkName: "board" */ './views/BoardView.vue')
