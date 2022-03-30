4c4d9891a968d60f73de2b35551af2aa96cff8b1
Noel Carmona <hello@noelcarmona.com>
2022-03-30 17:36:41 +0200

    [NAVBAR] Subscribed boards appears in navbar

--- src/layouts/RegisteredUserLayout.vue
+++ src/layouts/RegisteredUserLayout.vue
@@ -9,33 +9,57 @@
 
 <script lang="ts">
 import { AdjustmentsIcon, ArchiveIcon, DocumentAddIcon, LogoutIcon, UserIcon, ViewBoardsIcon } from '@heroicons/vue/solid'
-import { defineComponent } from '@vue/runtime-core'
+import { defineComponent, onBeforeMount } from '@vue/runtime-core'
 import NavBarComponent from '../components/UI/NavBar/NavBarComponent.vue'
 import { INavBar } from '../components/UI/NavBar/interfaces/NavBar'
-import { toRefs } from 'vue'
+import { ref, Ref, toRefs } from 'vue'
+import { INavBarSubMenu } from 'components/UI/NavBar/interfaces/NavBarSubMenu'
+import { Store, useStore } from 'vuex'
+import { INavBarSubLink } from 'components/UI/NavBar/interfaces/NavBarSubLink'
+import { RouteLocationRaw } from 'vue-router'
 
 export default defineComponent({
   components: {  NavBarComponent },
   setup(props) {
-    const menuItems: INavBar = {
+    const store:Store<any> = useStore()
+    
+    const menuItems: Ref<INavBar> = ref({
       left: [
         { text: "Create task", icon: DocumentAddIcon, type: "link" },
         { text: "Backlog", icon: ArchiveIcon, type: "link" },
         { text: "Board configuration", icon: AdjustmentsIcon, type: "link" },
       ],
       right: [
-        { text: "Boards", icon: ViewBoardsIcon, type: "menu", subLinks: [
-          { text: "Submenu1" },
-          { text: "Submenu2" },
-          { text: "Submenu3" }
-        ]},
         { text: "User", icon: UserIcon, type: "menu", subLinks: [
-          { text: "Submenu1" },
-          { text: "Submenu2" }
+          { text: "Submenu1", url: 'first-board' },
+          { text: "Submenu2", url: 'first-board' }
         ]},
         { text: "Logout", icon: LogoutIcon, type: "link" }
       ]
+    })
+    const getProfileBoards = ():string[] => store.getters.getProfileBoards
+    const formatBoardsURL = (boardName: string): string => boardName.replace(' ', '-')
+
+    const injectBoardsList = (): INavBarSubMenu => {
+      const submenuLinks:INavBarSubLink[] = getProfileBoards().map((b:string) => {
+        const board:string = formatBoardsURL(b)
+        return {
+          text: b,
+          url: {
+            name: 'board',
+            params: { board }
+          }
+        }
+      })
+
+      const submenu: INavBarSubMenu = {
+        text: "Boards", icon: ViewBoardsIcon, type: "menu", subLinks: submenuLinks, link: ''
+      }
+      return submenu
     }
+
+    onBeforeMount(() => menuItems.value.right.unshift(injectBoardsList()))
+
     return { menuItems, ...toRefs(props) }
   }
 })
