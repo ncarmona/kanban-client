a8e4ed63c819bc3b0a06185b17c241cb6d55fbdd
Noel Carmona <hello@noelcarmona.com>
2022-03-30 10:35:51 +0200

    [VUEX] Added profile module to store.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex';
 import boards from './boards'
+import profile from './profile'
 
 export default createStore({
-  modules: { boards }
+  modules: { boards, profile }
 })
