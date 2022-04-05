85057088ca1dd857ce1047c9ab68209e4f67896b
Noel Carmona <hello@noelcarmona.com>
2022-04-05 18:35:19 +0200

    [NAVBAR] Added submenu-item class to router-link in submenu

--- src/components/UI/NavBar/NavBarSubMenu/NavBarSubMenuComponent.vue
+++ src/components/UI/NavBar/NavBarSubMenu/NavBarSubMenuComponent.vue
@@ -7,7 +7,7 @@
     <div class="submenu mt-2 md:pl-0 md:flex md:flex-col pl-5 delay-75 duration-300 md:bg-white md:rounded md:shadow-md md:absolute md:z-10 md:top-11" v-show="menuVisible">
       <div class="md:text-gray-700 p-3 hover:bg-violet-700 hover:text-white" v-for="link in subLinks" :key="link.text">
         <a v-if="isStringURL(link.url)" :href="link.url" class="submenu-item">{{link.text}}</a>
-        <router-link v-else :to="link.url">{{link.text}}</router-link>
+        <router-link v-else :to="link.url" class="submenu-item">{{link.text}}</router-link>
       </div>
     </div>
   </div>
