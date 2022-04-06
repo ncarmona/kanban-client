383544a41938fac879c2c922b4fb9c85a733b3dc
Noel Carmona <hello@noelcarmona.com>
2022-04-06 10:33:47 +0200

    [HOME] Homeview lists all user suscribed boards.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,35 @@
 <template>
   <HomeLayout>
     <h1>Home</h1>
+    <ul>
+      <li v-for="board in boards" :key="board">
+        <router-link :to="boardLink(board)">{{board}}</router-link>
+      </li>
+    </ul>
   </HomeLayout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import HomeLayout from '../layouts/HomeLayout.vue'
+import { useStore } from 'vuex'
+import { RouteLocationRaw } from 'vue-router'
 
 export default defineComponent({
-  components: { HomeLayout }
+  components: { HomeLayout },
+  setup() {
+    const store = useStore()
+    const boards = computed(():string[] => store.getters.getProfileBoards)
+    const boardLink = (board: string):RouteLocationRaw => {
+      return {
+        name: 'board',
+        params: {
+          board: formatBoardsURL(board)
+        }
+      }
+    }
+    const formatBoardsURL = (boardName: string): string => boardName.replace(' ', '-')
+    return { boards, boardLink }
+  }
 })
 </script>
