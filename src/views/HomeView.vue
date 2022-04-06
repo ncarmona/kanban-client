e93d0a97d8f11a5bdd00d690a6600922d6b134cb
Noel Carmona <hello@noelcarmona.com>
2022-04-06 08:38:07 +0200

    [HOME] HomeView uses HomeLayout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Home</h1>
+  <HomeLayout>
+    <h1>Home</h1>
+  </HomeLayout>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HomeLayout from '../layouts/HomeLayout.vue'
+
+export default defineComponent({
+  components: { HomeLayout }
+})
+</script>
