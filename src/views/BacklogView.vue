2cf3487b8e919d686820c5261af7dc6357f65862
Noel Carmona <hello@noelcarmona.com>
2022-04-07 13:52:31 +0200

    [BACKLOG] BacklogView uses TaskGroup.

--- src/views/BacklogView.vue
+++ src/views/BacklogView.vue
@@ -1,30 +1,44 @@
 <template>
   <BoardLayout>
-    <div class="ml-4 mr-4">
-      <TaskComponent v-for="task in tasks" :key="task.id" :task="task" />
+    <div class="flex flex-col">
+      <p class="flex justify-center w-full mb-7">
+        Click a task in order to move it to sprint or backlog
+      </p>
+      <div class="flex md:flex-row flex-col grow">
+        <TaskGroupComponent class="w-11/12 md:w-6/12 ml-4 mr-4 mb-10" :backlog="backlog" textHeader="Backlog" field="unselected" />
+        <TaskGroupComponent class="w-11/12 md:w-6/12 ml-4 mr-4" :backlog="backlog" textHeader="Sprint" field="selected" />
+      </div>
     </div>
   </BoardLayout>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { defineComponent, Ref, ref } from 'vue'
 import BoardLayout from '../layouts/BoardLayout.vue'
-import TaskComponent from '../components/Task/Task/TaskComponent.vue'
+import TaskGroupComponent from '../components/TaskGroup/TaskGroupComponent.vue'
 import { useStore } from 'vuex'
 import { useRoute } from 'vue-router'
 import { IBoard } from 'domain/interfaces/IBoard'
+import ButtonComponent from '../components/UI/Button/ButtonComponent.vue'
+import { IButton } from 'components/UI/Button/interfaces/IButton'
 
 export default defineComponent({
-  components: { BoardLayout, TaskComponent },
+  components: { BoardLayout, TaskGroupComponent, ButtonComponent },
   setup() {
     const store = useStore()
     const route = useRoute()
     const board: string = route.params.board as string
+
     const getBoardName = (board:string):string => board.replace('-', ' ')
     const getCurrentBoard = (): IBoard => store.getters.getBoard(getBoardName(board))
-    const tasks = computed(() => getCurrentBoard().backlog)
-
-    return { tasks }
+    const buttonData: IButton = {
+      label: "Add to board"
+    } 
+    const backlog: Ref = ref({
+      selected: [],
+      unselected: [...getCurrentBoard().backlog]
+    })
+    return { backlog, buttonData }
   }
 })
 </script>
