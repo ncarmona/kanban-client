6e4747c2db95a75bd2a5c033ba14f5e284b2e3d1
Noel Carmona <hello@noelcarmona.com>
2022-04-07 13:52:02 +0200

    [BACKLOG] Task group component for backlog task.

---
+++ src/components/TaskGroup/TaskGroupComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <HeaderComponent class="mb-3 text-center" :headerNumber="3" :text="textHeader"/>
+    <TaskComponent @click="swapTaskBacklog(task)" v-for="task in backlog[field]" :key="task.id" :task="task" />
+  </div>
+</template>
+
+<script lang="ts">
+import { ITask } from 'domain/interfaces/ITask'
+import { defineComponent } from 'vue'
+import TaskComponent from '../Task/Task/TaskComponent.vue'
+import HeaderComponent from '../UI/Header/HeaderComponent.vue'
+
+export default defineComponent({
+  components: { TaskComponent, HeaderComponent },
+  props: {
+    backlog: {
+      required: true,
+      type: Object
+    },
+    textHeader: {
+      required: true,
+      type: String
+    },
+    field: {
+      required: true,
+      type: String
+    }
+  },
+  setup(props) {
+    const currentField = props.field
+    const targetField = props.field === 'selected' ? 'unselected' : 'selected'
+
+    const swapTaskBacklog = async(task:ITask) => {
+      props.backlog[currentField] = props.backlog[currentField].filter((t:ITask) => t.id !== task.id)
+      props.backlog[targetField].push(task)
+    }
+
+    return { swapTaskBacklog }
+  },
+})
+</script>
