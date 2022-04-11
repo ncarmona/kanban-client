c0e6289cf978e5bc118cab270d77d9ba23c707a7
Noel Carmona <hello@noelcarmona.com>
2022-04-11 13:34:38 +0200

    [UI] Button component uses processing state.

--- src/components/UI/Button/ButtonComponent.vue
+++ src/components/UI/Button/ButtonComponent.vue
@@ -1,15 +1,15 @@
 <template>
-  <button class="bg-violet-700 hover:bg-violet-500 text-white rounded p-2"
+  <button ref="btn" class="bg-violet-700 hover:bg-violet-500 text-white rounded p-2 disabled:cursor-not-allowed"
           :type="type"
           :name="name"
           :autofocus="autofocus"
           :disabled="disabled">
-    {{label}}
+    <span ref="btnSpan">{{visibleLabel}}</span>
   </button>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, toRefs } from 'vue'
+import { computed, defineComponent, ref, Ref, watch } from 'vue'
 import { IButton } from './interfaces/IButton'
 export default defineComponent({
   props: {
@@ -20,13 +20,36 @@ export default defineComponent({
   },
   setup(props) {
     const type = computed(() => props.button.type ?? 'button')
-    const autofocus = computed(() => props.button.autofocus ?? false)
-    const disabled = computed(() => props.button.disabled ?? false)
-    const name = computed(() => props.button.name ?? '')
-    const value = computed(() => props.button.value ?? '')
-    const label = computed(() => props.button.label)
+    const label:string = props.button.label
+    const disabled:Ref<boolean> = ref(props.button.disabled ?? false)
+    const autofocus:Ref<boolean> = ref(props.button.autofocus ?? false)
+    const name:Ref<string> = ref(props.button.name ?? '')
+    const value:Ref<string> = ref(props.button.value ?? '')
 
-    return { type, autofocus, disabled, name, value, label }
+    const visibleLabel:Ref = ref(label)
+    const btn:Ref = ref(null)
+    const btnSpan:Ref = ref(null)
+
+    const enableProcessing = () => {
+      visibleLabel.value = props.button.processing?.processingText
+      btnSpan.value.classList.add('animate-pulse')
+      btn.value.classList.add('cursor-progress')
+      btnSpan.value.classList.remove('disabled:cursor-not-allowed')
+    }
+
+    const disableProcessing = () => {
+      visibleLabel.value = label
+      btnSpan.value.classList.remove('animate-pulse')
+      btn.value.classList.remove('cursor-progress') 
+      btnSpan.value.classList.add('disabled:cursor-not-allowed')
+    }
+
+    watch(props.button.processing!, () => {
+      const { enabled } = props.button.processing!
+      enabled ? enableProcessing() : disableProcessing()
+    })
+
+    return { type, autofocus, disabled, name, value, visibleLabel, btn, btnSpan }
   },
 })
 </script>
