ead84e1f97e784cc6bb22586c90a7b094c45264a
Noel Carmona <hello@noelcarmona.com>
2022-04-11 17:23:18 +0200

    [UI] Removed value field in IButton

--- src/components/UI/Button/interfaces/IButton.ts
+++ src/components/UI/Button/interfaces/IButton.ts
@@ -1,11 +1,11 @@
 import { ButtonType } from './IButtonType'
 import { IProcessing } from './IProcessing'
+
 export interface IButton {
   autofocus?: boolean
   disabled?: boolean
   name?: string
   type?: ButtonType,
-  value?:string
   processing?: IProcessing
   label: string
 }
