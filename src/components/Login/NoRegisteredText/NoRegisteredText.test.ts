76815bb6afc48f9f03068ea1df258ea43e5b3b26
Noel Carmona <hello@noelcarmona.com>
2022-04-16 12:31:15 +0200

    [LOGIN] Displays no registered text span.

---
+++ src/components/Login/NoRegisteredText/NoRegisteredText.test.ts
@@ -0,0 +1,15 @@
+import { mount, VueWrapper } from '@vue/test-utils'
+import NoRegisteredTextComponent from './NoRegisteredTextComponent.vue'
+
+describe('NoRegosteredText component', () => {
+  let wrapper:VueWrapper = mount(NoRegisteredTextComponent)
+  const text:string = 'Have not registered?'
+  
+  beforeEach(() => wrapper = mount(NoRegisteredTextComponent))
+
+  it('No registered text rendered', () => {
+    const wrapperText:string = wrapper.find('span').text()
+
+    expect(wrapperText).toStrictEqual(wrapperText)
+  })
+})
---
+++ src/components/Login/NoRegisteredText/NoRegisteredTextComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <span class="font-extralight mr-1">Have not registered? </span>
+    <router-link class="text-violet-700" :to="signupRoute">Sign up now</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { RouteLocationRaw } from 'vue-router'
+
+export default defineComponent({
+  setup() {
+    const signupRoute:RouteLocationRaw = { name: 'login' }
+
+    return { signupRoute }
+  }
+})
+</script>
