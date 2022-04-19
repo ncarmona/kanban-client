c0187fd7345eaf16e9303c70fe5651cc5e78674d
Noel Carmona <hello@noelcarmona.com>
2022-04-19 09:48:24 +0200

    [LOGIN] Created AuthLayout for login and register view.

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex h-screen">
+    <div class="justify-center md:w-5/12 hidden md:block bg-violet-700 p-10">
+      <img :src="AuthLogo" alt="kanban auth logo" />
+    </div>
+    <div class="flex flex-col md:w-7/12 p-10 w-full">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AuthLogo from '../assets/kanban_auto.svg'
+
+export default defineComponent({
+  setup() {
+    return { AuthLogo }
+  }
+})
+</script>
