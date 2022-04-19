c86e5a20b7e09e98abb0bd7737a6599990e746b3
Noel Carmona <hello@noelcarmona.com>
2022-04-19 13:28:51 +0200

    [REGISTER] Added Register form and Header in registerView.

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,14 +1,27 @@
 <template>
   <AuthLayout>
-    <h1>Register view</h1>
+    <AlreadyHaveAnAccountComponent class="flex justify-end"/>
+      <div class="flex flex-col place-items-center w-full h-screen mt-10">
+        <HeaderComponent class="mb-8" :headerNumber="1" text="register" />
+        <RegisterFormComponent class="w-full md:w-6/12" :success="registerSuccess" :fail="registerFail"/>
+      </div>
   </AuthLayout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import AuthLayout from '../layouts/AuthLayout.vue'
+import HeaderComponent from '../components/UI/Header/HeaderComponent.vue'
+import RegisterFormComponent from '../components/Register/RegisterForm/RegisterFormComponent.vue'
+import AlreadyHaveAnAccountComponent from '../components/Register/AlreadyHaveAnAccount/AlreadyHaveAnAccountComponent.vue'
 
 export default defineComponent({
-  components: { AuthLayout }
+  components: { AuthLayout, HeaderComponent, RegisterFormComponent, AlreadyHaveAnAccountComponent },
+  setup() {
+    const registerSuccess = () => {}
+    const registerFail = () => {}
+
+    return { registerSuccess, registerFail }
+  }
 })
 </script>
