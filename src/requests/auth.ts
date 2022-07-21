195b87a6f0e60e5f8ab1ef465139096d3a6b9189
Noel Carmona <hello@noelcarmona.com>
2022-07-21 12:16:50 +0200

    [AUTH] Created auth interface

---
+++ src/requests/auth.ts
@@ -0,0 +1,11 @@
+import { Request } from '../core/request'
+import { IAuth } from 'domain/interfaces/IAuth'
+
+export class AuthRequests extends Request {
+  constructor() {
+    super("kanban-api", "/auth")
+  }
+  signup(credentials: IAuth):Promise<any> {
+    return this.connection!.post("/signup", credentials)
+  }
+}
