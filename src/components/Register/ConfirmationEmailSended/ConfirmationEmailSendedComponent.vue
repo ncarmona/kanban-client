eeb538ad752536a3cdd33045b91722b2f378f87d
Noel Carmona <hello@noelcarmona.com>
2022-07-25 17:59:49 +0200

    [AUTH] Confirmation of email sended component

---
+++ src/components/Register/ConfirmationEmailSended/ConfirmationEmailSendedComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <ShieldCheckIcon class="w-full md:w-4/12 text-green-500"/>
+  <span>Wait for email confirmation in
+    <span class="font-bold text-violet-700">{{email}}</span>. If you can not see the confirmation email in your inbox check your spam folder.
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs } from 'vue'
+import { ShieldCheckIcon } from '@heroicons/vue/outline'
+export default defineComponent({
+  props: {
+    email: {
+      required: true,
+      type: String
+    }
+  },
+  components: { ShieldCheckIcon },
+  setup(props) {
+    return { ...toRefs(props)}
+  },
+})
+</script>
