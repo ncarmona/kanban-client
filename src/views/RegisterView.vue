ce7a3aaeb1c8d9497ce5c70c77ed755fbe3bb2d9
Noel Carmona <hello@noelcarmona.com>
2022-07-25 18:00:19 +0200

    [AUTH] Once the register success inform to user.

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,27 +1,33 @@
 <template>
   <AuthLayout>
-    <AlreadyHaveAnAccountComponent class="flex justify-end"/>
+    <AlreadyHaveAnAccountComponent v-if="pendingToRegister" class="flex justify-end"/>
       <div class="flex flex-col place-items-center w-full h-screen mt-10">
-        <HeaderComponent class="mb-8" :headerNumber="1" text="register" />
-        <RegisterFormComponent class="w-full md:w-6/12" :success="registerSuccess" :fail="registerFail"/>
+        <div v-if="pendingToRegister">
+          <HeaderComponent class="mb-8" :headerNumber="1" text="register" />
+          <RegisterFormComponent class="w-full md:w-6/12" :success="registerSuccess" :fail="registerFail"/>
+        </div>
+        <ConfirmationEmailSendedComponent v-else email="email@gmail.com" />
       </div>
   </AuthLayout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent, ref, Ref } from 'vue'
 import AuthLayout from '../layouts/AuthLayout.vue'
 import HeaderComponent from '../components/UI/Header/HeaderComponent.vue'
 import RegisterFormComponent from '../components/Register/RegisterForm/RegisterFormComponent.vue'
 import AlreadyHaveAnAccountComponent from '../components/Register/AlreadyHaveAnAccount/AlreadyHaveAnAccountComponent.vue'
-
+import ConfirmationEmailSendedComponent from '../components/Register/ConfirmationEmailSended/ConfirmationEmailSendedComponent.vue'
 export default defineComponent({
-  components: { AuthLayout, HeaderComponent, RegisterFormComponent, AlreadyHaveAnAccountComponent },
+  components: { AuthLayout, HeaderComponent, RegisterFormComponent, AlreadyHaveAnAccountComponent, ConfirmationEmailSendedComponent },
   setup() {
-    const registerSuccess = () => {}
+    const registerOk: Ref<boolean> = ref(false)
+    const registerSuccess = () => {
+      registerOk.value = true
+    }
     const registerFail = () => {}
-
-    return { registerSuccess, registerFail }
+    const pendingToRegister = computed(() => !registerOk.value)
+    return { registerSuccess, registerFail, pendingToRegister }
   }
 })
 </script>
